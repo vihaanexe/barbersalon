d9c50bd3e3583bf30acf266f1d7accc6e5f2f70e
vineet <vsvihaan.exe@gmail.com>
2025-07-29 21:57:25 +0530

    Initial commit for Spring Boot backend

---
+++ src/main/java/com/barbersalon/barbersalon/BarbersalonApplication.java
@@ -0,0 +1,13 @@
+package com.barbersalon.barbersalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbersalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbersalonApplication.class, args);
+	}
+
+}
